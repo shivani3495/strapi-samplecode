4ac9b28559c4c4a6c8f1eff2ab32c66c7c7aa15e
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2022-07-06 09:14:58 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-sudo rm -f /var/www/html/index.html
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 12.14.1
+
+nvm use 12.14.1
+
+#create our working directory if it doesnt exist
+DIR="/home/ubuntu/strapi"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
